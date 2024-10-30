81453dfed98abde49f9e072eb4abbbdf37ef658b
Brian Fernandez <br14n_sol@protonmail.com>
2024-10-30 17:49:02 -0500

    docs: update `recursive` example

--- examples/recursive.js
+++ examples/recursive.js
@@ -1,34 +1,35 @@
-import select from '@inquirer/select'
-
+import select, { Separator } from '@inquirer/select'
 import fileSelector from '../dist/index.js'
 
-async function askForFile() {
-  let selectedFile = await select({
-    message: 'How do you want to proceed?',
+async function promptForPathSelection() {
+  let selectedOption = await select({
+    message: 'How do you want to select the path?',
     choices: [
-      { name: 'Select a file with a file selector', value: 'file-selector' },
-      { name: "Exit (I'm done)", value: 'exit' }
+      { name: 'File Selector', value: 'file-selector' },
+      // ... other options
+      new Separator(),
+      { name: "Exit", value: 'exit' }
     ]
   })
 
-  if (selectedFile === 'file-selector') {
-    selectedFile = await fileSelector({
+  if (selectedOption === 'file-selector') {
+    selectedOption = await fileSelector({
       message: 'Select a file:',
-      match: (file) => file.name.endsWith('.json'),
+      filter: (file) => file.isDirectory() || file.name.endsWith('.json'),
       allowCancel: true
     })
   }
 
-  if (selectedFile === 'canceled') {
-    return askForFile()
+  if (selectedOption === 'canceled') {
+    return promptForPathSelection()
   }
 
-  return selectedFile
+  return selectedOption
 }
 
 (async () => {
-  let filePath = await askForFile();
-  while (filePath !== 'exit') {
-    filePath = await askForFile();
+  let chosenPath = await promptForPathSelection()
+  while (chosenPath !== 'exit') {
+    chosenPath = await promptForPathSelection()
   }
-})();
+})()
