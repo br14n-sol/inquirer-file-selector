006b1e4f11e9993f11f1d715c3f3723d7dbf4eac
Brian Fernandez <br14n_sol@protonmail.com>
2024-12-21 14:30:52 -0500

    build: move the build process to `rollup` (#39)
    (cherry picked from commit 785861e605e9c32141008b0b378dfa81d59a7c10)

---
+++ rollup.config.ts
@@ -0,0 +1,32 @@
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import type { RollupOptions } from 'rollup'
+import { dts } from 'rollup-plugin-dts'
+import nodeExternals from 'rollup-plugin-node-externals'
+import tsConfigPaths from 'rollup-plugin-tsconfig-paths'
+
+import pkg from './package.json' with { type: 'json' }
+
+const { main, types } = pkg
+
+const bundle = (options: RollupOptions): RollupOptions => ({
+  ...options,
+  input: 'src/index.ts'
+})
+
+export default [
+  bundle({
+    output: {
+      file: main,
+      format: 'esm'
+    },
+    plugins: [nodeExternals(), typescript(), terser()]
+  }),
+  bundle({
+    output: {
+      file: types,
+      format: 'esm'
+    },
+    plugins: [nodeExternals(), tsConfigPaths(), dts()]
+  })
+]
