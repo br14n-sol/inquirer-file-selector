464f6bdc386209fe7c1d8dbcf04a86912773d354
Brian Fernandez <br14n_sol@protonmail.com>
2025-01-31 15:25:53 -0500

    refactor: replace `stripAnsiEscapeCodes` function with native utility

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,21 +1,17 @@
+import { stripVTControlCharacters } from 'node:util'
+
 /**
  * ANSI escape code to hide the cursor.
  */
 export const ANSI_HIDE_CURSOR = '\x1B[?25l'
 
 /**
- * Strip ANSI escape codes from a string.
- */
-export function stripAnsiEscapeCodes(str: string): string {
-  return str.replace(/\x1B\[\d+m/g, '')
-}
-
-/**
  * Computes the maximum length of the given strings after stripping ANSI escape codes.
  */
 export function getMaxLength(arr: string[]): number {
   return arr.reduce(
-    (maxLength, str) => Math.max(maxLength, stripAnsiEscapeCodes(str).length),
+    (maxLength, str) =>
+      Math.max(maxLength, stripVTControlCharacters(str).length),
     0
   )
 }
