300ead0c4a4757f755ff21ed18d7f2ae3dc38cc6
Brian Fernandez <br14n_sol@protonmail.com>
2025-03-31 15:48:18 -0500

    wip

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,7 @@ export const ANSI_HIDE_CURSOR = '\x1B[?25l'
 /** Possible prompt statuses. */
 export const Status = {
   Idle: 'idle',
+  Loading: 'loading',
   Done: 'done',
   Canceled: 'canceled'
 } as const
