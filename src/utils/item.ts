4032ae4fe78b8a52ba360be133f147c0c2f6d2ff
Brian Fernandez <br14n_sol@protonmail.com>
2025-05-19 23:23:50 -0500

    feat: return sanitized Item object without internal-only properties (#96)

--- src/utils/item.ts
+++ src/utils/item.ts
@@ -1,14 +1,14 @@
 import { readdirSync, statSync } from 'node:fs'
 import { basename, join, sep } from 'node:path'
-import type { Item } from '#types/item'
+import type { Item, RawItem } from '#types/item'
 
-/** Ensures the path ends with a separator (`/` or `\`). */
+/** Appends the system-specific path separator to the end of the path if missing. */
 export function ensurePathSeparator(path: string): string {
   return path.endsWith(sep) ? path : `${path}${sep}`
 }
 
-/** Creates an `Item' object from a file path. */
-export function createItemFromPath(path: string): Item {
+/** Creates a `RawItem` from a given filesystem path. */
+export function createRawItem(path: string): RawItem {
   const stats = statSync(path)
   const name = basename(path)
   const isDirectory = stats.isDirectory()
@@ -26,22 +26,24 @@ export function createItemFromPath(path: string): Item {
   }
 }
 
-/** Get items from a directory. */
-export function getDirItems(path: string): Item[] {
+/** Reads all entries in the directory and returns them as `RawItem[]`. */
+export function readRawItems(path: string): RawItem[] {
   return readdirSync(path).map(fileName => {
     const filePath = join(path, fileName)
-    return createItemFromPath(filePath)
+    return createRawItem(filePath)
   })
 }
 
 /**
- * Sort an array of `Item` objects by the following criteria:
- * 1. Disabled items are placed at the end.
- * 2. Directories are placed before files.
- * 3. Alphabetically if the priorities match.
+ * Sorts the given array of `RawItem`s by the following criteria:
+ * 1. Enabled items before disabled ones.
+ * 2. Directories before files.
+ * 3. Alphabetical order (by name) if priorities match.
+ *
+ * Mutates the original array.
  */
-export function sortItems(items: Item[]): Item[] {
-  return items.sort((a, b) => {
+export function sortRawItems(items: RawItem[]): void {
+  items.sort((a, b) => {
     const aPriority = (a.isDisabled ? 2 : 0) + (a.isDirectory ? -1 : 0)
     const bPriority = (b.isDisabled ? 2 : 0) + (b.isDirectory ? -1 : 0)
 
@@ -51,4 +53,10 @@ export function sortItems(items: Item[]): Item[] {
 
     return a.name.localeCompare(b.name)
   })
+}
+
+/** Removes internal-only properties (`displayName` and `isDisabled`) from a `RawItem`. */
+export function stripInternalProps(raw: RawItem): Item {
+  const { displayName, isDisabled, ...item } = raw
+  return item
 }
