de138831cba695e45cd71440f85deb74a750782c
Brian Fernandez <br14n_sol@protonmail.com>
2025-07-29 19:43:46 -0500

    refactor(utils): rename Actions to defaultKeybinds and update action check functions
    - Rename `Actions` constant in `consts.ts` to `defaultKeybinds`
    - Update action check functions in `utils/actions.ts` to use `defaultKeybinds`

--- src/consts.ts
+++ src/consts.ts
@@ -8,18 +8,13 @@ export const Status = {
   Canceled: 'canceled'
 } as const
 
-/**
- * Actions that can be performed in the prompt.
- * Each action corresponds to a map of key names.
- *
- * TODO: Put this in the config directly in a different pull request. See #68
- */
-export const Actions = {
-  Up: ['up', 'w'],
-  Down: ['down', 's'],
-  Back: ['left', 'a'],
-  Forward: ['right', 'd'],
-  Toggle: ['space'],
-  Confirm: ['enter', 'return'],
-  Cancel: ['escape']
-} as const
+/** Default keybinds used in the prompt. */
+export const defaultKeybinds = {
+  up: ['up', 'w'],
+  down: ['down', 's'],
+  back: ['left', 'a'],
+  forward: ['right', 'd'],
+  toggle: ['space'],
+  confirm: ['enter', 'return'],
+  cancel: ['escape']
+}
--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,30 +1,30 @@
 import type { KeypressEvent } from '@inquirer/core'
-import { Actions } from '#consts.js'
+import { defaultKeybinds } from '#consts.js'
 
 export function isUp(key: KeypressEvent): boolean {
-  return Actions.Up.some(k => k === key.name)
+  return defaultKeybinds.up.some(k => k === key.name)
 }
 
 export function isDown(key: KeypressEvent): boolean {
-  return Actions.Down.some(k => k === key.name)
+  return defaultKeybinds.down.some(k => k === key.name)
 }
 
 export function isBack(key: KeypressEvent): boolean {
-  return Actions.Back.some(k => k === key.name)
+  return defaultKeybinds.back.some(k => k === key.name)
 }
 
 export function isForward(key: KeypressEvent): boolean {
-  return Actions.Forward.some(k => k === key.name)
+  return defaultKeybinds.forward.some(k => k === key.name)
 }
 
 export function isToggle(key: KeypressEvent): boolean {
-  return Actions.Toggle.some(k => k === key.name)
+  return defaultKeybinds.toggle.some(k => k === key.name)
 }
 
 export function isConfirm(key: KeypressEvent): boolean {
-  return Actions.Confirm.some(k => k === key.name)
+  return defaultKeybinds.confirm.some(k => k === key.name)
 }
 
 export function isCancel(key: KeypressEvent): boolean {
-  return Actions.Cancel.some(k => k === key.name)
+  return defaultKeybinds.cancel.some(k => k === key.name)
 }
