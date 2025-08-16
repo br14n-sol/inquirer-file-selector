eca29536c5aa64d791d7ee6b6c7660d2de5ba9b2
Brian Fernandez <br14n_sol@protonmail.com>
2025-08-16 17:34:40 -0500

    chore: pass tsconfig directly to rollup-plugin-dts (#122)

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from 'rollup'
 import { dts } from 'rollup-plugin-dts'
 import nodeExternals from 'rollup-plugin-node-externals'
 import pkg from './package.json' with { type: 'json' }
-import tsConfig from './tsconfig.json' with { type: 'json' }
 
 const { main, types } = pkg
-const { compilerOptions } = tsConfig
 
 // biome-ignore lint/style/noDefaultExport: Rollup expects a default export
 export default defineConfig([
@@ -18,6 +16,6 @@ export default defineConfig([
   {
     input: 'src/index.ts',
     output: { file: types, format: 'esm' },
-    plugins: [dts({ compilerOptions: { paths: compilerOptions.paths } })]
+    plugins: [dts({ tsconfig: 'tsconfig.json' })]
   }
 ])
