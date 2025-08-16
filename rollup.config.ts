fd1e1cba570771d69f34cc4c9765262e69ab8799
Brian Fernandez <br14n_sol@protonmail.com>
2025-08-15 22:29:51 -0500

    Rework build process (#120)

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,31 +1,23 @@
-import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
-import type { RollupOptions } from 'rollup'
+import { defineConfig } from 'rollup'
 import { dts } from 'rollup-plugin-dts'
 import nodeExternals from 'rollup-plugin-node-externals'
-import tsConfigPaths from 'rollup-plugin-tsconfig-paths'
 import pkg from './package.json' with { type: 'json' }
+import tsConfig from './tsconfig.json' with { type: 'json' }
 
 const { main, types } = pkg
+const { compilerOptions } = tsConfig
 
-const bundle = (options: RollupOptions): RollupOptions => ({
-  ...options,
-  input: 'src/index.ts'
-})
-
-export default [
-  bundle({
-    output: {
-      file: main,
-      format: 'esm'
-    },
-    plugins: [nodeExternals(), typescript(), terser()]
-  }),
-  bundle({
-    output: {
-      file: types,
-      format: 'esm'
-    },
-    plugins: [nodeExternals(), tsConfigPaths(), dts()]
-  })
-]
+// biome-ignore lint/style/noDefaultExport: Rollup expects a default export
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: { file: main, format: 'esm' },
+    plugins: [nodeExternals(), typescript({ removeComments: true })]
+  },
+  {
+    input: 'src/index.ts',
+    output: { file: types, format: 'esm' },
+    plugins: [dts({ compilerOptions: { paths: compilerOptions.paths } })]
+  }
+])
