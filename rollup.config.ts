c576e534eb32d507ea0d962c769de94435226067
Brian Fernandez <br14n_sol@protonmail.com>
2025-08-15 22:19:52 -0500

    chore: use defineConfig instead of bundle util

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,5 @@
 import typescript from '@rollup/plugin-typescript'
-import type { RollupOptions } from 'rollup'
+import { defineConfig } from 'rollup'
 import { dts } from 'rollup-plugin-dts'
 import nodeExternals from 'rollup-plugin-node-externals'
 import pkg from './package.json' with { type: 'json' }
@@ -8,25 +8,22 @@ import tsConfig from './tsconfig.json' with { type: 'json' }
 const { main, types } = pkg
 const { compilerOptions } = tsConfig
 
-const bundle = (options: RollupOptions): RollupOptions => ({
-  ...options,
-  input: 'src/index.ts'
-})
-
 // biome-ignore lint/style/noDefaultExport: Rollup expects a default export
-export default [
-  bundle({
+export default defineConfig([
+  {
+    input: 'src/index.ts',
     output: {
       file: main,
       format: 'esm'
     },
     plugins: [nodeExternals(), typescript({ removeComments: true })]
-  }),
-  bundle({
+  },
+  {
+    input: 'src/index.ts',
     output: {
       file: types,
       format: 'esm'
     },
     plugins: [dts({ compilerOptions: { paths: compilerOptions.paths } })]
-  })
-]
+  }
+])
